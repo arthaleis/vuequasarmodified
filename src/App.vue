9e7b9de1e54146c8ba00475530186b8cd1acbc4e
Sabda Ramadhan <136231047+sabdaramadhan@users.noreply.github.com>
2023-10-10 07:56:16 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,151 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <!-- <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        /> -->
+
+        <q-toolbar-title>
+          Bayar SPP Sabda Ramadhan XII RPL 2
+        </q-toolbar-title>
+
+
+      </q-toolbar>
+    </q-header>
+
+    <!-- <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Docs</q-item-label>
+            <q-item-label caption>quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Registrasi Siswa/Petugas/Admin</q-item-label>
+            <q-item-label caption>Registrasi</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="chat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Discord Chat Channel</q-item-label>
+            <q-item-label caption>chat.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="forum" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Forum</q-item-label>
+            <q-item-label caption>forum.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
+          <q-item-section avatar>
+            <q-icon name="rss_feed" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Twitter</q-item-label>
+            <q-item-label caption>@quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+
+      <div class="q-pa-md" style="max-width: 400px">
+
+<q-form
+  @submit="onSubmit"
+  @reset="onReset"
+  class="q-gutter-md"
+>
+  <q-input
+    filled
+    v-model="name"
+    label="Your name *"
+    hint="Name and surname"
+    lazy-rules
+    :rules="[ val => val && val.length > 0 || 'Please type something']"
+  />
+
+  <q-input
+    filled
+    type="number"
+    v-model="age"
+    label="Your age *"
+    lazy-rules
+    :rules="[
+      val => val !== null && val !== '' || 'Please type your age',
+      val => val > 0 && val < 100 || 'Please type a real age'
+    ]"
+  />
+
+  <q-toggle v-model="accept" label="I accept the license and terms" />
+
+  <div>
+    <q-btn label="Submit" type="submit" color="primary"/>
+    <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+  </div>
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn color="primary" icon="mail" label="On Left" />
+    <q-btn color="secondary" icon-right="mail" label="On Right" />
+    <q-btn color="red" icon="mail" icon-right="send" label="On Left and Right" />
+    <br>
+    <q-btn icon="phone" label="Stacked" stack glossy color="purple" />
+  </div>
+</q-form>
+
+</div>
+    </q-drawer> -->
+
+    <q-page-container>
+      <HelloWorld />
+      <RegSis />
+    </q-page-container>
+  </q-layout>
+  
+  
+</template>
+
+<script>
+import { ref } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import RegSis from './components/RegSis.vue'
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+    // HelloWorld,
+    RegSis
+  },
+
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
+    }
+  }
+}
+</script>
---
+++ src/components/RegSis.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="q-pa-md" style="max-width: 400px; text-align: center; margin-left: auto; margin-right: auto;">
+
+    <q-form
+        @submit="onSubmit"
+        @reset="onReset"
+        class="q-gutter-md"
+    >
+        <q-input
+        filled
+        v-model="name"
+            label="Nama Anda"
+        hint="Tuliskan Nama Anda"
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        />
+
+        <q-input
+        filled
+        type="password"
+        v-model="password"
+            label="Masukkan Password Pengguna"
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        />
+
+        <q-input
+        filled
+        v-model="kelas"
+            label="Input Kelas"
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        />
+
+        
+
+
+        <q-toggle v-model="accept" label="I accept the license and terms" />
+
+        <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+    </q-form>
+    </div>
+
+    <div class="q-pa-md q-gutter-sm">
+    <q-banner rounded :class="$q.dark.isActive ? 'bg-grey-0' : 'bg-grey-'">
+    <template v-slot:avatar>
+        <img
+        src="../assets/aldo.jpg"
+        style="width: auto; height: 400px;margin-left:170px;"
+        >
+        <img
+        src="../assets/emenqui.jpg"
+        style="width: auto; height: 400px;margin-left: 15px;"
+        >
+        <img
+        src="../assets/tidursaja.jpg"
+        style="width: auto; height: 400px;margin-left: 15px;"
+        >
+        <img
+        src="../assets/videographerganteng.jpg"
+        style="width: auto; height: 400px;margin-left: 15px;"
+        >
+    </template>
+
+
+    <template v-slot:action>    
+    </template>
+    </q-banner>
+</div>
+
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
+
+export default {
+    setup () {
+    const $q = useQuasar()
+
+    const name = ref(null)
+    const age = ref(null)
+    const accept = ref(false)
+
+    return {
+        name,
+        age,
+        accept,
+
+        onSubmit () {
+        if (accept.value !== true) {
+            $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'You need to accept the license and terms first'
+            })
+        }
+        else {
+            $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted'
+            })
+        }
+        },
+
+        onReset () {
+        name.value = null
+        age.value = null
+        accept.value = false
+        }
+    }
+    }
+}
+</script>
